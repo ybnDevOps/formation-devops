c6da392d556f11912a054845977962333c827b8c
Alice Bob <alice@eni.fr>
2018-10-05 10:10:48 +0200

    ajout nickname

--- tests/demo.tests.js
+++ tests/demo.tests.js
@@ -1,10 +1,12 @@
 var should = require("should");
-var User = function(name){
+var User = function(name, nickname){
     this.name = name;
+    this.nickname = nickname;
 }; 
 describe("Checking if the user is created correctly", function(){
     it("should create the user with the correct name", function(){
-        var tom = new User("tom");
-        tom.name.should.be.equal("tommy");
+        var tom = new User("tom", "tommy");
+        tom.name.should.be.equal("tom");
+        tom.nickname.should.be.equal("tommy");
     });
 });
