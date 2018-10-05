514530d99e1feb086fa21e9430b4806a75e7aec2
Alice Bob <alice@eni.fr>
2018-10-05 09:28:29 +0200

    skeleton + tests

---
+++ tests/demo.tests.js
@@ -0,0 +1,10 @@
+var should = require("should");
+var User = function(name){
+    this.name = name;
+}; 
+describe("Checking if the user is created correctly", function(){
+    it("should create the user with the correct name", function(){
+        var tom = new User("tom");
+        tom.name.should.be.equal("tom");
+    });
+});
