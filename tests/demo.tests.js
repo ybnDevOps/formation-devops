cc7a9ad6bc6184645ef52706301ec5dbb7ed8fe8
Alice Bob <alice@eni.fr>
2018-10-05 09:52:25 +0200

    update compare tom name

--- tests/demo.tests.js
+++ tests/demo.tests.js
@@ -5,6 +5,6 @@ var User = function(name){
 describe("Checking if the user is created correctly", function(){
     it("should create the user with the correct name", function(){
         var tom = new User("tom");
-        tom.name.should.be.equal("tom");
+        tom.name.should.be.equal("tommy");
     });
 });
